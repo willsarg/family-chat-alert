d9d8128c44d9a6fd9d57c0b74accc6eb81e1cfea
Will Sarg <12886992+willsarg@users.noreply.github.com>
2025-05-25 18:42:10 -0400

    .env fix

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
 from .routes import bp as api_bp
-from pathlib import Path
 
 def create_app():
     app = Flask(__name__)
-    env_path = Path(__file__).parent.parent / ".env"
-    app.config.from_pyfile(str(env_path))
     app.register_blueprint(api_bp, url_prefix="/api")
 
     return app
