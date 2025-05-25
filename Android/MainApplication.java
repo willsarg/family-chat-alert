bc88c2efe728f8cf444239758a0677ec111e65e7
Will Sarg <12886992+willsarg@users.noreply.github.com>
2025-05-25 13:36:41 -0400

    adding android code

---
+++ Android/MainApplication.java
@@ -0,0 +1,29 @@
+public static class MainApplication extends Application implements ReactApplication {
+
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            @SuppressWarnings("UnnecessaryLocalVariable")
+            List<ReactPackage> packages = new PackageList(this).getPackages();
+            // Add your custom package here
+            packages.add(new SmsListenerPackage());
+            return packages;
+        }
+    };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
+    }
+}
